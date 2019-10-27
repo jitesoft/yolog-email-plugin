5778658d3746e930da66a82b1f2bec944226e424
Johannes Tegnér <johannes@jitesoft.com>
2019-10-27 21:16:29 +0100

    Test implementation.

---
+++ tests/Plugin.test.js
@@ -0,0 +1,96 @@
+import Plugin from '../src/index';
+import nodemailer from 'nodemailer';
+
+const plugin = new Plugin(
+  ['test@example.com', 'test@localhost', 'Test McTest <test@local>'],
+  'Tester McTestington the third <test@localhost>', '{TAG} - {DATETIME}',
+  [nodemailer.createTransport({ jsonTransport: true })]);
+
+describe('Tests for the Yolog email plugin.', () => {
+  test('Emails are sent to recipients.', async () => {
+    const result = await plugin.log('error', Date.now(), 'Message!', new Error('...'));
+    const resultAsObject = JSON.parse(result[0].value.message);
+
+    expect(resultAsObject.to).toHaveLength(3);
+    expect(resultAsObject.to).toEqual([
+      {
+        address: 'test@example.com',
+        name: ''
+      },
+      {
+        address: 'test@localhost',
+        name: ''
+      },
+      {
+        address: 'test@local',
+        name: 'Test McTest'
+      }
+    ]);
+  });
+
+  test('Sender email is correct.', async () => {
+    const result = await plugin.log('error', Date.now(), 'Message!', new Error('...'));
+    const resultAsObject = JSON.parse(result[0].value.message);
+    expect(resultAsObject.from).toEqual({
+      name: 'Tester McTestington the third',
+      address: 'test@localhost'
+    });
+  });
+
+  test('Message is correctly parsed as HTML.', async () => {
+    const d = new Date();
+    const tag = 'error';
+    const message = 'Hi, this is the message!';
+    const error = new Error('An error!');
+    const stacktrace = error.stack;
+    const result = await plugin.log(tag, d, message, error);
+    const resultAsObject = JSON.parse(result[0].value.message);
+
+    expect(resultAsObject.html).toEqual(`<div>
+    <span style="font-size: 1.4em; text-decoration: underline;">${tag} message logged.</span>
+    <pre>At: ${d.toISOString()}</pre>
+    <div style="padding-top: 1em;">
+        <span style="text-decoration: underline; line-height: 0.4em;">Message:</span>
+        <p>${message}</p>
+    </div>
+    <pre style="border: 1px black dotted; font-size: 1em; width: max-content; padding: 1em 3em;">${stacktrace}</pre>
+    <span style="font-size: 0.6em;">This message was logged via the <a style="text-decoration: none;" href="https://www.npmjs.com/package/@jitesoft/yolog"><code>@jitesoft/yolog</code></a> email plugin.</span>
+</div>`
+    );
+  });
+
+  test('Message is correctly parsed as Text.', async () => {
+    const d = new Date();
+    const tag = 'error';
+    const message = 'Hi, this is the message!';
+    const error = new Error('An error!');
+    const stacktrace = error.stack;
+    const result = await plugin.log(tag, d, message, error);
+    const resultAsObject = JSON.parse(result[0].value.message);
+
+    expect(resultAsObject.text).toEqual(`Logged a log message with ${tag} tag at ${d.toISOString()}.\n\nMessage: ${message}\n\nCallstack:\n${stacktrace}`);
+  });
+
+  test('Message is correctly parsed as none-default HTML.', async () => {
+    plugin.setHtmlTemplate('<a href="abc123">hej! {TAG}</a>');
+    const result = await plugin.log('error', Date.now(), 'anything', new Error());
+    const resultAsObject = JSON.parse(result[0].value.message);
+
+    expect(resultAsObject.html).toEqual('<a href="abc123">hej! error</a>');
+  });
+
+  test('Message is correctly parsed as none-default Text.', async () => {
+    plugin.setTextTemplate('Hej! {TAG} {MESSAGE}');
+    const result = await plugin.log('error', Date.now(), 'anything', new Error());
+    const resultAsObject = JSON.parse(result[0].value.message);
+
+    expect(resultAsObject.text).toEqual('Hej! error anything');
+  });
+
+  test('Subject is correct.', async () => {
+    const date = new Date();
+    const result = await plugin.log('error', date, 'anything', new Error());
+    const resultAsObject = JSON.parse(result[0].value.message);
+    expect(resultAsObject.subject).toEqual(`error - ${date.toISOString()}`);
+  });
+});
